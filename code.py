9bb40fb75eaead49f9c4fbb431d0e9d67311eb3d
houcem <houcem.ellouze.28325972@gmail.com>
2023-12-29 21:40:07 +0100

    Second commit

--- code.py
+++ code.py
@@ -1,4 +1,20 @@
 from better_profanity import profanity
-text = "This is freaking ridiculous, what the hell is wrong with you?"
-censored = profanity.censor(text)
-print(censored)
+
+def censurer_texte(texte):
+    censure = profanity.censor(texte)
+    return censure
+
+# Exemple 1
+texte1 = "Ce type est vraiment un imbécile, je ne peux pas le supporter !"
+resultat1 = censurer_texte(texte1)
+print("Exemple 1:")
+print("Texte original:", texte1)
+print("Texte censuré:", resultat1)
+print()
+
+# Exemple 2
+texte2 = "Je n'aime pas quand les choses ne fonctionnent pas correctement, c'est vraiment frustrant."
+resultat2 = censurer_texte(texte2)
+print("Exemple 2:")
+print("Texte original:", texte2)
+print("Texte censuré:", resultat2)
